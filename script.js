0b7412d75f55000ca3f35763124004dbc00501a0
TasnimulASC21 <tislam@mecps.org>
2021-08-06 16:51:35 -0400

    first commit

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ let anime_list = ['Bloodlad', 'Kekkai Sensen', 'No Game No Life', 'Overlord'];
 
 
 let old_header = document.getElementById('outaku');
-
+let reccom_header = document.createElement("h1");
 reccomandations.onclick = function(){
     content_div.remove();
     old_header.remove();
-    let reccom_header = document.createElement("h1");
     body.appendChild(reccom_header);
     reccom_header.innerHTML = "Top 10 Anime List"; 
     
@@ -24,6 +23,7 @@ reccomandations.onclick = function(){
 
     for (let i = 0; i < anime_list.length; i++){
         let newElement = document.createElement("p");
+        newElement.id = 'anime_list';
         newElement.innerHTML = anime_list[i];
         newElement.style["textAlign"] = "center";
         newElement.style["fontSize"] = "30px";
@@ -34,7 +34,14 @@ reccomandations.onclick = function(){
 let body = document.querySelector("body");
 let home = document.getElementById("home");
 
-home.onclick = function(){
+home.addEventListener("click", click);
+
+function click(){
     reccom_header.remove();
+    body.appendChild(old_header);
     body.appendChild(content_div);
+    for (let i = 0; i < anime_list.length; i++){
+        let list = document.getElementById('anime_list');
+        list.remove();
+    }
 }
